f43b4eb5804ba8dfd078ac7a1e341e6a399e3980
cherr <charity.grey1@gmail.com>
2026-01-02 15:34:36 -0700

    modify more spacing

--- pwa_app/src/components/DailyHabits.tsx
+++ pwa_app/src/components/DailyHabits.tsx
@@ -25,7 +25,7 @@ function DailyHabits() {
   }
 
   return (
-    <div className="flex flex-wrap flex-row gap-4">
+    <div className="flex flex-wrap flex-row justify-around gap-4">
       {habits.map((habit, i) => {
         if (habit.MaxLevels) {
           return (
--- pwa_app/src/components/NumericHabit.tsx
+++ pwa_app/src/components/NumericHabit.tsx
@@ -9,6 +9,31 @@ export default function NumericHabit(props: NumericHabitProps) {
     return (
         <div className="flex flex-col justify-center items-center">
              <label>{props.habit.Habit}</label>
+        <div className="bg-[#E7745F] h-15 w-15 my-2 rounded-2xl flex flex-col justify-center items-center shadow-xl pointer-events-none transition-transform duration-200">
+           
+            <input
+                className="text-white font-bold text-center pointer-events-none bg-transparent w-12"
+                type="number"
+                value={props.habit.Completed}
+                min={0}
+                max={props.habit.MaxLevels}
+                onChange={(e) => {
+                    const completed = parseInt(e.target.value, 10);
+                    props.updateItem(props.habit.Habit, { Completed: completed });
+                }}
+            />
+            
+        
+        </div>
+        </div>
+    );
+}
+
+
+function NumericHabitv1(props: NumericHabitProps) {
+    return (
+        <div className="flex flex-col justify-center items-center">
+             <label>{props.habit.Habit}</label>
         <div className="bg-[#E7745F] h-15 w-15 my-2 rounded-full flex justify-center items-center flex-col shadow-lg">
            
             <input
