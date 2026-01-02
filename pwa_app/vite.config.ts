dbcb9d78828674b3f6b8071639d41c19eb36c513
cherr <charity.grey1@gmail.com>
2026-01-01 23:31:38 -0700

    add pwa functionality

--- pwa_app/vite.config.ts
+++ pwa_app/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { VitePWA, type ManifestOptions, type VitePWAOptions } from "vite-plugin-pwa";
+import manifestObject from './manifest.json'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+
+const manifestForPlugin : Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  "includeAssets": [], //["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+  manifest: manifestObject as Partial<ManifestOptions>,
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugin )],
 })
