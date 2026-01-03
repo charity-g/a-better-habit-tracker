9204248d1acc49ed3066b4df1d0f66b7f6ba9979
cherr <charity.grey1@gmail.com>
2026-01-02 17:52:46 -0700

    add glass to binaryHabit as well

--- pwa_app/src/components/BinaryHabit.tsx
+++ pwa_app/src/components/BinaryHabit.tsx
@@ -10,7 +10,7 @@ export default function BinaryHabit(props: BinaryHabitProps) {
     return (
         <div className="flex flex-col justify-center items-center">
              <label>{props.habit.Habit}</label>
-        <div className="bg-[#E7745F] h-15 w-15 my-2 rounded-2xl flex flex-col justify-center items-center shadow-xl pointer-events-none transition-transform duration-200">
+        <div className="glass-card h-15 w-15 my-2 rounded-2xl flex flex-col justify-center items-center shadow-xl transition-transform duration-200">
             <input type="checkbox"
                 checked={props.habit.Completed === 1}
                 onChange={(e) => {
