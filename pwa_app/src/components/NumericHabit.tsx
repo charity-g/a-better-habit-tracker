d74230371a33ac20fd6509c248bdb5d3c1c47fd8
cherr <charity.grey1@gmail.com>
2026-01-02 17:33:47 -0700

    add snap carosel skeleton

--- pwa_app/src/components/NumericHabit.tsx
+++ pwa_app/src/components/NumericHabit.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { dailyHabits } from "../types/types";
 
 interface NumericHabitProps {
@@ -6,24 +7,21 @@ interface NumericHabitProps {
 }
 
 export default function NumericHabit(props: NumericHabitProps) {
+    const [focus, setFocus] = useState(props.habit.Completed);
+
     return (
         <div className="flex flex-col justify-center items-center">
              <label>{props.habit.Habit}</label>
-        <div className="glass-card h-15 w-15 my-2 rounded-2xl flex flex-col justify-center items-center shadow-xl pointer-events-none transition-transform duration-200">
-           
-            <input
-                className="text-white font-bold text-center pointer-events-none bg-transparent w-12"
-                type="number"
-                value={props.habit.Completed}
-                min={0}
-                max={props.habit.MaxLevels}
-                onChange={(e) => {
-                    const completed = parseInt(e.target.value, 10);
-                    props.updateItem(props.habit.Habit, { Completed: completed });
-                }}
-            />
-            
-        
+        <div className="glass-card h-15 w-15 my-2 rounded-2xl flex flex-col justify-center items-center shadow-xl transition-transform duration-200">
+            <div className="h-full w-full overflow-hidden">
+                <div className="flex flex-col overflow-x-auto scroll-smooth snap-mandatory hide-scrollbar-global">
+
+                    {Array.from({ length: (props.habit.MaxLevels) as number + 1 }, (_, index) => index).map((level) => (
+                (<div className="shrink-0 h-full w-full snap-center snap-always flex flex-col justify-center items-center">
+                    <p className="text-center text-lg font-semibold text-white">{level}</p>
+                </div>)))}
+            </div>
+            </div>
         </div>
         </div>
     );
@@ -37,7 +35,7 @@ function NumericHabitv1(props: NumericHabitProps) {
         <div className="bg-[#E7745F] h-15 w-15 my-2 rounded-full flex justify-center items-center flex-col shadow-lg">
            
             <input
-                className="text-white font-bold text-center pointer-events-none bg-transparent w-12"
+                className="text-white font-bold text-center pointer-events-auto bg-transparent w-12"
                 type="number"
                 value={props.habit.Completed}
                 min={0}
