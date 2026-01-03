1dea8e5f42583a87d58aeb52199c0a21564efd6e
cherr <charity.grey1@gmail.com>
2026-01-03 10:31:27 -0700

    add input and scroll

--- pwa_app/src/components/NumericHabit.tsx
+++ pwa_app/src/components/NumericHabit.tsx
@@ -10,21 +10,43 @@ export default function NumericHabit(props: NumericHabitProps) {
     const [focus, setFocus] = useState(props.habit.Completed);
 
     return (
-        <div className="flex flex-col justify-center items-center">
-             <label>{props.habit.Habit}</label>
-        <div className="glass-card h-15 w-15 my-2 rounded-2xl flex flex-col justify-center items-center shadow-xl transition-transform duration-200">
+    <div className="flex flex-col justify-center items-center">
+        <div className="glass-card 
+                        h-15 w-15 my-2 rounded-2xl 
+                        flex justify-center items-center 
+                        shadow-xl transition 
+                        focus-within:ring-2 
+                        focus-within:ring-white/70 
+                        focus-within:ring-offset-2 
+                        focus-within:ring-offset-transparent"
+           >
          <input type="number"
                 className="text-center text-2xl font-semibold text-white bg-transparent outline-none"
                 min={0}
                 max={props.habit.MaxLevels || 1}
                 value={focus}
-                onFocus={() => setFocus(props.habit.Completed)}
+                onFocus={(e) => {
+                    e.currentTarget.focus();
+                    setFocus(props.habit.Completed)}}
                 onChange={(e) => {
                     const completed = parseInt(e.target.value) || 0;
                     setFocus(completed);
                 }}
+                onWheel={(e) => {
+                    e.preventDefault();
+                    e.currentTarget.focus();
+
+                    const delta = e.deltaY < 0 ? 1 : -1;
+                    setFocus((prev) => {
+                    const next = prev + delta;
+                    const min = 0;
+                    const max = props.habit.MaxLevels || 1;
+                    return Math.min(max, Math.max(min, next));
+                    });
+                }}
             />
         </div>
-        </div>
+        <label>{props.habit.Habit}</label>
+    </div>
     );
 }
