afda7594ac97df5a924deff34ed8c44ffacec1d1
cherr <charity.grey1@gmail.com>
2026-01-04 11:33:30 -0800

    center on screen

--- pwa_app/src/App.tsx
+++ pwa_app/src/App.tsx
@@ -7,8 +7,8 @@ import PrayerTracker from './components/PrayerTracker.tsx'
 function App() {
   return (
     <div className="h-screen w-screen custom-background">
-      <div className='h-screen'>
-         <DailyHabits />
+      <div className='h-screen flex flex-col gap-6 justify-center items-center'>
+        <DailyHabits />
         <TaskTracker />
       </div>
          <WeeklyHabits />
