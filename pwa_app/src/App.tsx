102bbf3b7b07c6626a1bc7f2504881ae9017be01
cherr <charity.grey1@gmail.com>
2026-01-05 23:05:01 -0800

    try with google login

--- pwa_app/src/App.tsx
+++ pwa_app/src/App.tsx
@@ -1,27 +1,36 @@
 import './App.css'
+import { GoogleLogin, type CredentialResponse } from '@react-oauth/google';
 import DailyHabits from './components/DailyHabits.tsx'
 import TaskTracker from './components/TaskTracker.tsx'
 // import WeeklyHabits from './components/WeeklyHabits.tsx'
 // import PrayerTracker from './components/PrayerTracker.tsx'
 
 
+
 function App() {
-  return (
-    <AppLoggedIn />
-  )
+    const responseMessage = (response: CredentialResponse) => {
+        console.log(response);
+    };
+    const errorMessage = () => {
+        console.log("Login Failed, Google does not return any information, so no idea why it failed.");
+    };
+    return (
+        <div className='h-screen custom-background flex justify-center items-center'>
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+            
+         {/* <WeeklyHabits /> */}
+        {/* <PrayerTracker /> */}
+        </div>
+    )
 }
 
 function AppLoggedIn() {
   
   return (
-    <div className="h-screen custom-background flex justify-center items-center">
         <div className='max-w-sm flex flex-col gap-6 justify-center items-center'>
         <DailyHabits />
         <TaskTracker />
       </div>
-         {/* <WeeklyHabits /> */}
-        {/* <PrayerTracker /> */}
-    </div>
   )
 }
 
