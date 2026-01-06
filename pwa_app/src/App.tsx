bd1a46c353cde7b0ebb4914014d7123a7c83b8c6
cherr <charity.grey1@gmail.com>
2026-01-05 22:46:40 -0800

    abstract app logged in

--- pwa_app/src/App.tsx
+++ pwa_app/src/App.tsx
@@ -4,11 +4,18 @@ import TaskTracker from './components/TaskTracker.tsx'
 // import WeeklyHabits from './components/WeeklyHabits.tsx'
 // import PrayerTracker from './components/PrayerTracker.tsx'
 
+
 function App() {
+  return (
+    <AppLoggedIn />
+  )
+}
+
+function AppLoggedIn() {
+  
   return (
     <div className="h-screen custom-background flex justify-center items-center">
         <div className='max-w-sm flex flex-col gap-6 justify-center items-center'>
-          
         <DailyHabits />
         <TaskTracker />
       </div>
